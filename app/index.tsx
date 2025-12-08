c7b28a9b749675372f3d5e242d4680e68ac10f38
ryan <nghiaqcqs@gmail.com>
2025-12-08 11:45:20 +0700

    Initial commit
    Generated by create-expo-app 3.5.3.

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+// import * as Sentry from "@sentry/react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{fontSize: 20}}>Let's crash remotely with Sentry!</Text>
+
+      {/* insert button here */}
+
+    </View>
+  );
+}
